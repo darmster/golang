6569d1be2c60610faa6dd6f316810ae156fae4f0
dar <darmish@gmail.com>
2020-01-04 18:56:49 -0600

    adding more

---
+++ tut/src/variables/short-variable-declarations.go
@@ -0,0 +1,13 @@
+// Inside a function, the := short assignment statement can be used in place of a var declaration with implicit type.
+
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
+}
---
+++ tut/src/variables/variables-with-initializers.go
@@ -0,0 +1,13 @@
+//A var declaration can include initializers, one per variable.
+
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java)
+}
+
